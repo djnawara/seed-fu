3553837306455fbeb570a954eac2f62b8dbb3c0c
Durran Jordan and Leo Soto <dev+durran+leosoto@hashrocket.com>
2010-06-22 12:50:26 -0400

    Fix spec_helper to run tests independently of a rails app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,23 @@
-require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
-
 plugin_spec_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+require "rubygems"
+require "active_record"
+require "seed-fu"
+
+ActiveRecord::Base.establish_connection({
+  :adapter => :mysql,
+  :host => "localhost",
+  :user => "root",
+  :database => "seed_fu_test"
+})
 ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
 
 class SeededModel < ActiveRecord::Base
   validates_presence_of :title
+end
+
+Spec::Runner.configure do |config|
+  config.before do
+    SeededModel.delete_all
+  end
 end
