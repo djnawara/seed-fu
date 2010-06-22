b8e30f5bc79379620bd9190f1c61a38de78e795c
Durran Jordan and Leo Soto <dev+durran+leosoto@hashrocket.com>
2010-06-22 13:42:17 -0400

    Remove require for hash_ext since we deleted it

--- lib/seed-fu/writer.rb
+++ lib/seed-fu/writer.rb
@@ -1,4 +1,3 @@
 require File.join( File.dirname(__FILE__), 'writer', 'abstract' )
 require File.join( File.dirname(__FILE__), 'writer', 'seed' )
 require File.join( File.dirname(__FILE__), 'writer', 'seed_many' )
-require File.join( File.dirname(__FILE__), 'writer', 'hash_ext' )
